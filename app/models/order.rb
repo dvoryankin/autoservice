a90442cb3c6965a95d2c0eeab05f3a756f222690
dvoryankin <dvoryankindm@gmail.com>
2022-07-13 17:28:34 +0300

    add default sorting by ids

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ApplicationRecord
   has_many :services
 
   validates :customer_name, presence: true
+
+  default_scope { order(created_at: :asc) }
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -2,4 +2,6 @@ class Service < ApplicationRecord
   belongs_to :order
   belongs_to :category
   has_one :performer
+
+  default_scope { order(created_at: :asc) }
 end
