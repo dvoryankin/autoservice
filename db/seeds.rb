5a270253d16a49a3f3fda6a0d810fcdb3a963be6
dvoryankin <dvoryankindm@gmail.com>
2022-07-14 23:48:49 +0300

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,10 @@
 end
 
 10.times do
+  Category.create( title: Faker::Space.planet)
+end
+
+10.times do
   Service.create( title: Faker::Color.color_name, order_id: rand(1..10), category_id: rand(1..10))
 end
 
@@ -16,18 +20,7 @@ end
   Performer.create( name: Faker::Artist.name, service_id: rand(1..10))
 end
 
-10.times do
-  Category.create( title: Faker::Space.planet)
-end
+Order.first.services << Service.first << Service.second << Service.third
 
+Service.first << Order.first << Order.second << Order.third
 
-
-s = Service.create(title:'test', order: Order.first)
-
-Order.first.services << Service.first
-
-# Order.first.services(true)
-
-Order.first.services.create(title: 'test1')
-
-# Order.first.services.find(4)
